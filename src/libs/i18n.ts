a976cc76a4afd7c17cc87a5e54db785fa9f2f61a
vBubblery <juncheng@flowlity.com>
2020-04-05 22:40:49 +0200

    update i18n

--- src/libs/i18n.ts
+++ src/libs/i18n.ts
@@ -1,11 +1,14 @@
 import NextI18Next from 'next-i18next';
 
+const languages = ['en', 'zh'];
+
 const NextI18NextInstance = new NextI18Next({
   defaultLanguage: 'en',
   otherLanguages: ['zh'],
   localePath: 'public/static/locales',
 });
 
+NextI18NextInstance.i18n.languages = languages;
 /* Optionally, export class methods as named exports */
 export const {
   appWithTranslation,
